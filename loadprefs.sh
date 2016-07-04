f9cd4ebe875e6b942e678f7446210e70ee0ef0fe
AlexLamson <alex.lamson@gmail.com>
2016-07-04 09:49:23 -0700

    added some more python libraries

--- loadprefs.sh
+++ loadprefs.sh
@@ -85,11 +85,12 @@ sudo apt-get -y -qq install colordiff
 sudo apt-get -y -qq install festival
 
 # Install some useful packages
-sudo apt-get -y -qq install build-essential sox  git curl xbitmaps
+sudo apt-get -y -qq install sox git curl xbitmaps
 sudo -H pip install e speedtest-cli when-changed
 
 
 # Install useful python libraries
+sudo apt-get -t -qq install build-essential python-dev python-numpy python-setuptools python-scipy libatlas-dev libatlas3-base
 sudo apt-get -y -qq install python-pyqt5 python3-pyqt5
 sudo apt-get -y -qq build-dep python-matplotlib && sudo apt-get -y -qq install python-matplotlib
 sudo apt-get -y -qq install python-numpy python-scipy scikit-learn
@@ -97,3 +98,4 @@ sudo apt-get -y -qq install python-numpy python-scipy scikit-learn
 # Install pybrain
 sudo apt-get -y -qq install ipython ipython-notebook python-pandas python-sympy python-nose
 sudo pip install pybrain
+
